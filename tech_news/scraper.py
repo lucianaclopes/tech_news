0e5c75c422ff81ad30032743ec06ff131b86d992
lucianaclopes <lucianacls@outlook.com>
2024-08-09 14:19:22 -0300

    req 3 scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -35,7 +35,13 @@ def scrape_updates(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    selector = Selector(text=html_content)
+    next_page = selector.css(
+        'a.next.page-numbers::attr(href)'
+        ).get()
+    if next_page:
+        return next_page
+    return None
 
 
 # Requisito 4
