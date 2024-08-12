7268ae01e924a720b5b30db630bc13df44108840
lucianaclopes <lucianacls@outlook.com>
2024-08-12 12:11:04 -0300

    req 8 search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,30 @@
+from tech_news.database import db
+from datetime import datetime
+
+
 # Requisito 7
 def search_by_title(title):
     """Seu código deve virnsitive aqui"""
-    raise NotImplementedError
+    title_insensitive = title.lower()
+    news = db.news.find(
+        {"title": {"$regex": title_insensitive, "$options": "i"}}
+    )
+    answer = [(item["title"], item["url"]) for item in news]
+    return answer
 
 
 # Requisito 8
 def search_by_date(date):
     """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    date_formatted = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+    news = db.news.find({"timestamp": {"$regex": date_formatted}})
+    answer = [(item["title"], item["url"]) for item in news]
+    return answer
 
 
 # Requisito 9
